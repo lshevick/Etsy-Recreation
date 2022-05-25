87c82be6e09cd3b108ff71a91864c96561d86e4f
Levi Shevick <levidshevick@gmail.com>
2022-05-25 11:22:17 -0400

    get handlebars template working

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,35 @@
 const source = document.querySelector('#item-template').innerHTML;
 const template = Handlebars.compile(source);
-const context = data.results;
-
-const html = template(context);
-
-document.querySelector('.items-card').innerHTML = html;
-
+console.log('data', data);
+const context = data; 
 console.log(context);
+const html = template(context); // are you passing in an object or an array
+
+
+document.querySelector('.card-container').innerHTML = html;
+console.log(html);
+
+
+// need to write a function or helper that will fill an item card
+// with each separate element of the card (e.g. title, price, image, etc.)
+
+
+
+//this loop gets each individual item from the results array in the data object
+//
+
+// Handlebars.registerHelper('loop', function (context, options) {
+//     let contextItem = '';
+
+//     for (let i = 0; i < context.length; i++) {
+//         contextItem = options.fn(context[i]);
+//     }
+//     console.log(contextItem);
+//     return contextItem;
+
+// })
+
+// for (let i = 0; i < context.length; i++) {
+//     let contextItem = context[i];
+//     console.log(contextItem.price);
+// }
