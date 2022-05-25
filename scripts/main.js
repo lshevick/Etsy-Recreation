b277c3feb332cbf32d3bcf7c62afed8f4b6c8430
Levi Shevick <levidshevick@gmail.com>
2022-05-25 14:00:33 -0400

    adds search bar functionality

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,22 @@ const symbol = document.querySelector('.currency-symbol');
 // need to get search bar input and filter the results from
 // the data.results object, then only display those
 
+function searchItems() {
+    const items = document.querySelectorAll('.item-card');
+    // const itemTitle = document.querySelectorAll('.item-title');
+    const searchInput = document.querySelector('#searchbox').value;
+    console.log(searchInput);
+
+        for (let i = 0; i < items.length; i++) {
+
+            if (items[i].innerHTML.toLowerCase()
+            .includes(searchInput.toLowerCase())) {
+                items[i].style.display = 'block';
+            } else {
+                items[i].style.display = 'none';
+                
+            }
+        }
+}
+
+document.addEventListener('keyup', searchItems);
