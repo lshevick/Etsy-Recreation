a8d7f5cc32bf0fc459179c517d86edd1912da402
Levi Shevick <levidshevick@gmail.com>
2022-05-25 13:30:14 -0400

    changes layout and adds searchbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,18 @@
 const source = document.querySelector('#item-template').innerHTML;
 const template = Handlebars.compile(source);
 console.log('data', data);
-const context = data; 
+const context = data;
 console.log(context);
 const html = template(context); // are you passing in an object or an array
 
 
+
 document.querySelector('.card-container').innerHTML = html;
 
+const symbol = document.querySelector('.currency-symbol');
+// need to write function to insert correct currency symbol depending on what the 
+// currency is. E.G
 
-// need to write a function or helper that will fill an item card
-// with each separate element of the card (e.g. title, price, image, etc.)
+// need to get search bar input and filter the results from
+// the data.results object, then only display those
 
-
-
-//this loop gets each individual item from the results array in the data object
-//
-
-// Handlebars.registerHelper('loop', function (context, options) {
-//     let contextItem = '';
-
-//     for (let i = 0; i < context.length; i++) {
-//         contextItem = options.fn(context[i]);
-//     }
-//     console.log(contextItem);
-//     return contextItem;
-
-// })
-
-// for (let i = 0; i < context.length; i++) {
-//     let contextItem = context[i];
-//     console.log(contextItem.price);
-// }
